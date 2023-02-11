94ea940c31a2e8efa85536d23ea5e2f9e383c0ce
Adam_Bricknell <adam_bricknell@live.com>
2023-02-11 16:41:03 +0000

    add tool actions

--- commands.py
+++ commands.py
@@ -2,21 +2,12 @@ import subprocess
 
 from langchain.agents import tool
 
+
 @tool
-def chrome_os_action(apple_script):
+def computer_action(apple_script):
     """
-    Use this when you want to control the computer. The command should be in AppleScript.
-
-    It is good for opening Chrome and interacting with other apps, but not for interacting with the Chrome itself.
-
+    Use this when you want to execute a command on the computer. The command should be in AppleScript.
     Here are some examples of good AppleScript commands:
-
-    Command: Create a new page in Notion
-    AppleScript: tell application "Notion"
-        activate
-        delay 0.5
-        tell application "System Events" to keystroke "n" using {{command down}}
-    end tell
 
     Command: Search for a table nearby
     AppleScript: tell application "Google Chrome"
@@ -24,12 +15,11 @@ def chrome_os_action(apple_script):
     open location "https://www.google.com/search?q=Table+nearby"
     end tell
 
-    The AppleScript should be valid and when appropriate use keyboard shortcuts.
-
+    The AppleScript should be valid and never use keyboard shortcuts.
     Write the AppleScript for the Command:
     """
-    print("Running AppleScript: ", apple_script)
     p = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(f'sending to terminal:\n{apple_script}')
     stdout, stderr = p.communicate(apple_script.encode('utf-8'))
 
     if p.returncode != 0:
@@ -37,49 +27,50 @@ def chrome_os_action(apple_script):
 
     return stdout
 
+
 @tool
-def chrome_javascript_action(javascript):
+def get_url_compose_gmail(url_extension):
     """
-    Use this when you want to execute a command in Chrome. The command should be Javascript.
+    Use this when you want to create a url extension to send an email with gmail.
+    Here are some examples of good url extensions:
 
-    You can also use the javascript commands to learn more about the page you're on.
+    Command: Write an email to john@example.com
+    url_extension: to=john@example.com
 
-    Here are some helpful Javascript commands:
+    Command: Write an email to adam@gmail.com
+    url_extension: to=adam@gmail.com
 
-    Command: Get the clickable elements on the page
-    Javascript: Array.from(document.querySelectorAll('a, button, input[type="submit"], input[type="button"]'))
-
-    Command: Click the button with the text "Sign in"
-    Javascript: document.querySelector('button:contains("Sign in")').click()
-
-    Command: Enter the text "Hello" into the input with the placeholder "Search"
-    Javascript: document.querySelector('input[placeholder="Search"]').value = "Hello"
-
-    The Javascript must be valid including quotes and semicolons.
-
-    Write the Javascript for the Command:
+    Command: Write an email to jess@house.com
+    url_extension: to=jess@house.com
+    
+    The URL should be valid and never use keyboard shortcuts.
+    Write the url_extension for the Command:
     """
-    print("Running Javascript: ", javascript)
 
-    p = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    initial_url = 'https://mail.google.com/mail/?view=cm&fs=1&'
+    url = initial_url + url_extension
+    return url
 
-    script = f'''
-    tell application "Google Chrome"
-        activate
-        tell active tab of front window
-            execute javascript "{javascript}"
-        end tell
-    end tell
-    '''
 
-    stdout, stderr = p.communicate(script.encode('utf-8'))
 
-    if p.returncode != 0:
-        raise Exception(stderr)
 
-    return stdout
-
+@tool
 def chrome_open_url(url):
+    """
+    Use this when you want to get a specific URL with Chrome. The command should be in AppleScript.
+
+    Here are some examples of good AppleScript commands:
+
+    Command: Search for a table nearby
+    AppleScript: tell application "Google Chrome"
+    activate
+    open location "https://www.google.com/search?q=Table+nearby"
+    end tell
+
+    The AppleScript should be valid and never appropriate use keyboard shortcuts.
+
+    Write the AppleScript for the Command:
+    """
     p = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     script = f'''
@@ -94,5 +85,42 @@ def chrome_open_url(url):
 
     return stdout
 
-if __name__ == '__main__':
-    chrome_open_url('https://gmail.com')
+
+@tool
+def chrome_action(javascript):
+    """
+    Use this when you want to execute a command in Chrome. The command should be Javascript.
+
+    You can also use the javascript commands to learn more about the page you're on.
+
+    Here are some helpful Javascript commands:
+
+    Command: Get the clickable elements on the page
+    Javascript: Array.from(document.querySelectorAll('a, button, input[type="submit"], input[type="button"]'))
+    
+    Command: Click the button with the text "Sign in"
+    Javascript: document.querySelector('button:contains("Sign in")').click()
+    
+    Command: Enter the text "Hello" into the input with the placeholder "Search"
+    Javascript: document.querySelector('input[placeholder="Search"]').value = "Hello"
+    
+    The Javascript must be valid including quotes and semicolons.
+    Write the Javascript for the Command:
+    """
+    p = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    script = f'''
+    tell application "Google Chrome"
+        activate
+            tell active tab of front window
+            execute javascript "{javascript}" in active tab of window 1
+        end tell
+    end tell
+    '''
+    stdout, stderr = p.communicate(script.encode('utf-8'))
+
+    if p.returncode != 0:
+        raise Exception(stderr)
+
+    return stdout
+
