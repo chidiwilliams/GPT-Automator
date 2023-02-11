97b8988c5bc8b31e240b45e9c292c431aa7d952a
Chidi Williams <williamschidi1@gmail.com>
2023-02-11 11:52:29 +0000

    Initial Commit

---
+++ Fizz/FizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzApp.swift
+//  Fizz
+//
+//  Created by Chidi Williams on 11/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
