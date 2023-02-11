4432e609a6556e910f13faa9baabb6c5bcd55dae
Luke Harries <lharries@users.noreply.github.com>
2023-02-11 13:28:07 +0000

    added command line call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # import load_dotenv
+import sys
+
 from dotenv import load_dotenv
 
 from langchain import PromptTemplate
@@ -11,36 +13,45 @@ from commands import action
 # load environment variables
 load_dotenv()
 
-# First, let's load the language model we're going to use to control the agent.
-llm = OpenAI(temperature=0.9)
+def main(command):
+    # First, let's load the language model we're going to use to control the agent.
+    llm = OpenAI(temperature=0.9)
 
-command = "Open the calculator app"
+    prompt = f"""
+    I want you to generate the AppleScript to execute a command.
 
-prompt = f"""
-I want you to generate the AppleScript to execute a command.
+    Here are some examples of good AppleScript commands:
 
-Here are some examples of good AppleScript commands:
+    Command: Create a new page in Notion
+    AppleScript: tell application "Notion"
+        activate
+        delay 0.5
+        tell application "System Events" to keystroke "n" using {{command down}}
+    end tell
 
-Command: Create a new page in Notion
-AppleScript: tell application "Notion"
+    Command: Search for a table nearby
+    AppleScript: tell application "Google Chrome"
     activate
-    delay 0.5
-    tell application "System Events" to keystroke "n" using {{command down}}
-end tell
+    open location "https://www.google.com/search?q=Table+nearby"
+    end tell
 
-Command: Search for a table nearby
-AppleScript: tell application "Google Chrome"
-  activate
-  open location "https://www.google.com/search?q=Table+nearby"
-end tell
+    The AppleScript should be valid and when appropriate use keyboard shortcuts.
 
-The AppleScript should be valid and when appropriate use keyboard shortcuts.
+    Write the AppleScript for the Command: {command}?
+    """
 
-Write the AppleScript for the Command: {command}?
-"""
+    applescript = llm(prompt)
 
-applescript = llm(prompt)
+    print("Prompt: ", applescript)
 
-applescript = applescript.replace("AppleScript: ", "")
+    applescript = applescript.replace("AppleScript: ", "")
 
-action(applescript)
+    action(applescript)
+
+if __name__ == "__main__":
+    command = sys.argv[1]
+    if not command:
+        print("Please provide a command to execute e.g. python main.py 'Open the calculator app'")
+        exit(1)
+
+    main(command)
